9d41143bb3122567e5c73af22c59a24d331168d9
hater <70040637+Aquilarift@users.noreply.github.com>
2022-04-30 20:20:21 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+$targetPath = "uploads/" . "1.jpg";
+move_uploaded_file($_FILES["inpFile"]["tmp_name"], $targetPath);
+
+$command = escapeshellcmd('python net.py');
+$output = shell_exec($command);
+echo $output;
+?>
